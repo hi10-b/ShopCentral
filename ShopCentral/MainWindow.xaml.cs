022a4273d2c955b6bbdd49dc694700c23a1c1ec6
hi10-b <41531756+hi10-b@users.noreply.github.com>
2021-07-10 16:03:49 +1000

    10.1.2

--- ShopCentral/MainWindow.xaml.cs
+++ ShopCentral/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net;
+using System.IO;
 
 namespace ShopCentral
 {
@@ -23,6 +25,51 @@ namespace ShopCentral
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            //Get Rewuest -----------------
+            //string urlPath = String.Format("https://jsonplaceholder.typicode.com/posts");
+            //WebRequest requestObjectGet = WebRequest.Create(urlPath);
+            //requestObjectGet.Method = "GET";
+            //HttpWebResponse responseObj = null;
+            //responseObj = (HttpWebResponse)requestObjectGet.GetResponse();
+
+            //string test = null;
+            //using (Stream stream = responseObj.GetResponseStream())
+            //{
+            //    StreamReader sr = new StreamReader(stream);
+            //    test = sr.ReadToEnd();
+            //    test_lbl.Content =  test;
+            //    sr.Close();
+            //}
+
+            //Post Request ------------------------------
+            string urlPath2 = String.Format("https://personal-hm11mvg2.outsystemscloud.com/ShopCentral/rest/ShopAPI/CreateItem");
+
+            string purchaseVal = purchaseTotal_textbox.Text;
+            string postData = "{\"Name\": \"Name3\",  \"Barcode\": \"1234567891234567\",  \"IsActive\": \"true\",  \"CategoryID\": \"1\",  \"Price\": \"$purchaseVal\"}";
+            WebRequest requestObjPost = WebRequest.Create(urlPath2);
+            requestObjPost.Method ="POST";
+            requestObjPost.ContentType = "application/json";
+
+            using(var streamWriter = new StreamWriter(requestObjPost.GetRequestStream()))
+            {
+                streamWriter.Write(postData);
+                streamWriter.Flush();
+                streamWriter.Close();
+
+                var httpResponse = requestObjPost.GetResponse();
+
+                using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                {
+                    
+                        var result2 = streamReader.ReadToEnd();
+                    test_lbl.Content = result2;
+                }
+            }
         }
     }
 }
